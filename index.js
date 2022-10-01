ca4b15338b74aa6ad8463bbf20e2a412244cf19c
Dicksen Veloopillay <dicksenveloopillay@Dicksens-MacBook-Pro.local>
2022-10-01 16:13:47 -0400

    server listen with log informations added

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ var imagesSave = require("save")("images");
 
 // Create the restify server
 server = restify.createServer({ name: SERVER_NAME });
+
+server.listen(PORT, HOST, function () {
+  console.log("Server %s listening at %s", server.name, server.url);
+  console.log("Endpoints:");
+  console.log("http://127.0.0.1:5000/images method: GET, POST, DELETE");
+});
