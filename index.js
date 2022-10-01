a3796ddee8151e0093184a55e908b21dbe743e67
Dicksen Veloopillay <dicksenveloopillay@Dicksens-MacBook-Pro.local>
2022-10-01 16:11:47 -0400

    require restify, imageSave collection and restify createserver added

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 var SERVER_NAME = "image-api";
 var PORT = 8000;
 var HOST = "127.0.0.1";
+
+var restify = require("restify");
+
+// Get a persistence engine for the images
+var imagesSave = require("save")("images");
+
+// Create the restify server
+server = restify.createServer({ name: SERVER_NAME });
