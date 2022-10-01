66f4ae4c675016db9b2cf0398e8e1126240772c2
Dicksen Veloopillay <dicksenveloopillay@Dicksens-MacBook-Pro.local>
2022-10-01 18:12:41 -0400

    request count logger function created

--- index.js
+++ index.js
@@ -27,3 +27,12 @@ server.use(restify.fullResponse());
 
 // Maps req.body to req.params so there is no switching between them
 server.use(restify.bodyParser());
+
+// Function to log GET and POST request counter
+const requestCountLogger = () => {
+  console.log(
+    "Processed Request Count--> Get:%s, Post:%s",
+    requestGetCounter,
+    requestPostCounter
+  );
+};
